071f8c53a687d26cfc81dc5fd7851d5d7c3cdc10
otmosina <otmosina@gmail.com>
2021-04-26 19:50:06 +0800

    add speedscope

---
+++ speedscope.rb
@@ -0,0 +1,13 @@
+# Stackprof report -> flamegraph in speedscope
+# ruby 17-stackprof-speedscope.rb
+require 'json'
+require 'stackprof'
+require_relative 'task-1.rb'
+
+p "start..."
+profile = StackProf.run(mode: :wall, raw: true, ignore_gc: true) do
+  work('data/data25.txt')
+end
+p "finish!"
+
+File.write('stackprof_reports/stackprof.json', JSON.generate(profile))
