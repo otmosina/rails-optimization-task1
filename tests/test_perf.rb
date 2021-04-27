a45b401cc41550522ff8a01165203be6f07fb97d
otmosina <otmosina@gmail.com>
2021-04-27 19:24:04 +0800

    Start perf imrpoving

--- tests/test_perf.rb
+++ tests/test_perf.rb
@@ -7,17 +7,14 @@ RSpec.configure do |config|
 end
 
 describe 'Perfomance' do
-    let(:size) { 100000 }
+    let(:size) { 10000 }
     it 'works under 20s' do
         expect{
             work("data/data#{size}.txt")
-            #work("data/data25.txt")
         }.to perform_under(10).sec.sample(3).times
     end
 
     it 'perform linear' do
         expect { |n, _i| work("data/data#{n}.txt") }.to perform_linear.in_range([10, 100, 1000, 10000, 100_000])
     end
-
-    
 end
